5ec5500dec0745eb9eb02503650da6888e73f227
ahmeddjebbara34-hue <ahmeddjebbara34@gmail.com>
2026-01-04 15:54:04 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1776,7 +1776,7 @@ text_slider.on('slideChangeTransitionStart', function () {
             const $el = $("#typing-text");
             if (!$el.length) return; // stop if element not exist
 
-            const words = ["Developer", "Visual Designer", "Crypto"];
+            const words = ["senior graphic designer", "packaging designer", "Brand Designer"];
             let index = 0;
             let letterIndex = 0;
             let isDeleting = false;
